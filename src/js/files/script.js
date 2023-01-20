a1ac3f45a876ebcd683a305cb314aa41bcbab963
SeddiMK <web.egorovm@gmail.com>
2023-01-20 17:57:42 +0300

    js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,3 +3,10 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 //=============================================
+function myFn() {
+	let a = 3
+	let b = 3
+	return a + b
+}
+myFn()
+console.log(myFn());

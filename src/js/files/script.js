e9ea3fd817e604d0dc1f7ef47730c9350d93efc7
SeddiMK <web.egorovm@gmail.com>
2023-01-21 14:18:24 +0300

    installDate

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,6 +6,19 @@ import { flsModules } from "./modules.js";
 const myFu = (value, c = 1) => {
 	return value * c
 }
-console.log(myFu(3, 9));
+//console.log(myFu(3, 9));
 var str = 'fgfggg';
-console.log(str[0]);
+//console.log(str[0]);
+//=============================================
+//Установка свойства в объект текущей даты
+const newPost = (post, addedAt = Date()) => ({
+	...post,
+	addedAt, // addedAt: addedAt,
+})
+const firstPost = {
+	id: 1,
+	author: 'Max',
+}
+newPost(firstPost)
+console.log(newPost(firstPost));
+console.log(newPost);

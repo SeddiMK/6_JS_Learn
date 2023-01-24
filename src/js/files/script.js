19b00387ebd1c918fb17d95a4f1d5cda5e9134d9
SeddiMK <web.egorovm@gmail.com>
2023-01-24 15:20:32 +0300

    new Class

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -84,3 +84,16 @@ Object.values(myObject).forEach(key => {
 //	y: true,
 //	z: 'abcasdasda'
 //})); 
+//=============================================
+class Comment {
+	constructor(text) {
+		this.text = text
+		this.votesQty = 0
+	}
+	upvote() {
+		this.votesQty += 1
+	}
+}
+
+const firstComment = new Comment('First comment')
+console.log(firstComment);
